819bfe0eb69e4fe462dece60639524793bfeac39
hanna <hnyblom@kth.se>
2018-09-24 22:44:49 +0200

    forstfarande 0 poäng :.(

--- player.cpp
+++ player.cpp
@@ -10,36 +10,7 @@
 namespace TICTACTOE {
 using namespace std;
 vector<vector<int> > ticTac(4, vector<int>(4));
-//vector<vector<int> > me(4, vector<int>(4));
-//vector<vector<int> > opponent(4, vector<int>(4));
-
-
-int lastPlayer =1;
 int pruning=0;
-
-
-//int fill(){
-//    ticTac[0][0]=3;
-//    ticTac[0][1]=2;
-//    ticTac[0][2]=2;
-//    ticTac[0][3]=3;
-//    
-//    ticTac[1][0]=2;
-//    ticTac[1][1]=4;
-//    ticTac[1][2]=4;
-//    ticTac[1][3]=2;
-//    
-//    ticTac[2][0]=2;
-//    ticTac[2][1]=4;
-//    ticTac[2][2]=4;
-//    ticTac[2][3]=2;
-//    
-//    ticTac[3][0]=3;
-//    ticTac[3][1]=2;
-//    ticTac[3][2]=2;
-//    ticTac[3][3]=3;
-//    
-//}
 
 //om raden och kolumnen är död, placera inte där
 //int isDead(int row, int col){
@@ -53,68 +24,7 @@ int pruning=0;
 //    } 
 //    return 0;
 //}
-//Döda chans på rad/kolumn/diagonal om motståndarre placerat där
-//int kill(int nr, int id){
-//    if(id==0){
-//        //kill row
-//        rows[id]=0;
-//        --rowChances;
-//    }else if(id==1){
-//        //kill col
-//        cols[id]=0;
-//        --colChances;
-//    }else if(id==2){
-//        //kill diag
-//        diag[id]=0;
-//        --diChances;
-//    }
-//    return 0;
-//}
 
-//Kollar om grannar och ger riktning
-string isNeighbours(int row1, int col1,  int row2, int col2){
-    if(abs(row1-row2)==1){
-        //Vertical
-        return "V";
-    }else if(abs(col1-col2)==1){
-        //Horisontal
-        return "H";
-    }else if(abs(row1-row2)==1&&abs(col1-col2)==1){
-        //Diagonal
-        return "D";
-    }else{
-        return "N";
-    }
-}
-//int place(int row, int col, int meOrOpp){
-//    if(meOrOpp==0){
-//        //me
-//        me[row][col]=1;
-//        if(row==col){
-//            //Diagonal1
-//            myScoreDi[0]+=1;        
-//        }else if(row==0&&col==3||row==1&&col==2||row==2&&col==1||row==3&&col==0){
-//            //Diagonal2
-//            myScoreDi[1]+=1;
-//        }else{
-//            myScoreRows[row]+=1;
-//            myScoreCols[col]+=1;
-//        }
-//    }else{
-//        opponent[row][col]=1;
-//        if(row==col){
-//            //Diagonal1
-//            opScoreDi[0]+=1;        
-//        }else if(row==0&&col==3||row==1&&col==2||row==2&&col==1||row==3&&col==0){
-//            //Diagonal2
-//            opScoreDi[1]+=1;
-//        }else{
-//            opScoreRows[row]+=1;
-//            opScoreCols[col]+=1;
-//        }
-//        
-//    }
-//}
 //Kollar om motståndaren är ett drag ifrån att vinna
 //tuple<int, string, string, string, int> danger(){
 //    tuple<int, string, string, string, int> final = make_tuple(0,"","","",0);
@@ -172,7 +82,87 @@ vector<GameState> mu(const GameState &pState){
     return lNextStates;
     
 }
-int gamma(int player, const GameState &pState){
+int rowDanger(const GameState &pState, int i, int j, int cell){
+    if(j==0){
+        if(pState.at(i,1)==cell && pState.at(i,2)==cell && pState.at(i,3)==cell){
+            return true;
+        }
+    }else if(j==1){
+        if(pState.at(i,0)==cell && pState.at(i,2)==cell && pState.at(i,3)==cell){
+            return true;
+        }
+    }else if(j==2){
+        if(pState.at(i,0)==cell && pState.at(i,1)==cell && pState.at(i,3)==cell){
+            return true;
+        }      
+    }else if(j==3){
+        if(pState.at(i,0)==cell && pState.at(i,1)==cell && pState.at(i,2)==cell){
+            return true;
+        }        
+    }
+    return false;
+}
+int colDanger(const GameState &pState, int i, int j, int cell){
+    if(j==0){
+        if(pState.at(1,j)==cell && pState.at(2,j)==cell && pState.at(3,j)==cell){
+            return true;
+        }
+    }else if(j==1){
+        if(pState.at(0,j)==cell && pState.at(2,j)==cell && pState.at(3,j)==cell){
+            return true;
+        }
+    }else if(j==2){
+        if(pState.at(0,j)==cell && pState.at(1,j)==cell && pState.at(3,j)==cell){
+            return true;
+        }
+        
+    }else if(j==3){
+        if(pState.at(0,j)==cell && pState.at(1,j)==cell && pState.at(2,j)==cell){
+            return true;
+        }        
+    }
+    return false;
+}
+int diDanger(const GameState &pState, int i, int j, int cell){
+    if(i==j==0){
+        if(pState.at(1,1)==cell && pState.at(2,2)==cell && pState.at(3,3)==cell){
+            return true;
+        }
+    }else if(i==j==1){
+        if(pState.at(0,0)==cell && pState.at(2,2)==cell && pState.at(3,3)==cell){
+            return true;
+        }
+    }else if(i==j==2){
+        if(pState.at(0,0)==cell && pState.at(1,1)==cell && pState.at(3,3)==cell){
+            return true;
+        }
+        
+    }else if(i==j==3){
+        if(pState.at(0,0)==cell && pState.at(1,1)==cell && pState.at(2,2)==cell){
+            return true;
+        }        
+    }
+    else if(i==0&&j==3){
+        if(pState.at(1,2)==cell && pState.at(2,1)==cell && pState.at(3,0)==cell){
+            return true;
+        }
+    }else if(i==1&&j==2){
+        if(pState.at(0,3)==cell && pState.at(2,1)==cell && pState.at(3,0)==cell){
+            return true;
+        }
+    }else if(i==2&&j==1){
+        if(pState.at(0,3)==cell && pState.at(1,2)==cell && pState.at(3,0)==cell){
+            return true;
+        }
+    }else if(i==3&&j==0){
+        if(pState.at(0,3)==cell && pState.at(1,2)==cell && pState.at(2,1)==cell){
+            return true;
+        }
+    }
+    return false;
+}
+
+int gamma(bool player, const GameState &pState){
     //Calculate value of state
     //Possible winning opportunities
     //cerr<<"\n------gamma----\n";
@@ -188,6 +178,12 @@ int gamma(int player, const GameState &pState){
     vector<int> yScoreCols(4);
     vector<int> xScoreDi(2);
     vector<int> yScoreDi(2);
+    vector<int> xStopRowDanger(4);
+    vector<int> xStopColDanger(4);
+    vector<int> yStopRowDanger(4);
+    vector<int> yStopColDanger(4);
+    vector<int> xStopDiDanger(2);
+    vector<int> yStopDiDanger(2);
 
     int xRowChances =4;
     int xColChances =4;
@@ -208,33 +204,46 @@ int gamma(int player, const GameState &pState){
     
     yDi[0]=1;
     yDi[1]=1;
-    cerr<<"yRowChances: "<<yRowChances<<"\n";
+    //cerr<<"yRowChances: "<<yRowChances<<"\n";
     for(int i=0;i<4;++i){
-        for(int j=0;j<4;++j){
-            
+        for(int j=0;j<4;++j){            
             if(pState.at(i,j)==CELL_X){
-                
+                if(rowDanger(pState, i, j, CELL_O)){
+                    xStopRowDanger[i]=1;
+                }
+                if(colDanger(pState, i, j, CELL_O)){
+                    xStopColDanger[i]=1;
+                }
                 if(yRows[i]!=0){
                    yRows[i]=0;
                    --yRowChances;
-                   cerr<<"yRowChances: "<<yRowChances<<"\n";
+                   //cerr<<"yRowChances: "<<yRowChances<<"\n";
                 }
                 if(yCols[j]!=0){
                   yCols[j]=0;
                   --yColChances;
-                  cerr<<"yColChances: "<<yColChances<<"\n";
+                  //cerr<<"yColChances: "<<yColChances<<"\n";
                 }
                              
                 int diT = isDiag(i, j);
+                if(diDanger(pState,i,j,CELL_O)){
+                    xStopDiDanger[diT]=1;
+                }
                 if(diT!=2){
                     if(yDi[diT]!=0){
                         yDi[diT]=0;
                         --yDiChances;
-                        cerr<<"yDiChances: "<<yDiChances<<"\n";
+                        //cerr<<"yDiChances: "<<yDiChances<<"\n";
                     }
                }
             }
             if(pState.at(i,j)==CELL_O){
+                if(rowDanger(pState, i, j, CELL_X)){
+                    yStopRowDanger[i]=1;
+                }
+                if(colDanger(pState, i, j, CELL_X)){
+                    yStopColDanger[i]=1;
+                }
                 if(xRows[i]!=0){
                     xRows[i]=0;
                     --xRowChances;
@@ -245,6 +254,9 @@ int gamma(int player, const GameState &pState){
                 }
                 
                 int diT = isDiag(i, j);
+                if(diDanger(pState,i,j,CELL_X)){
+                    yStopDiDanger[diT]=1;
+                }
                 if(diT!=2){
                     if(xDi[diT]!=0){
                         xDi[diT]=0;
@@ -261,6 +273,8 @@ int gamma(int player, const GameState &pState){
                 //X ensam på rad/column
                 if(xRows[i]!=0){xScoreRows[i]+=1;}
                 if(xCols[j]!=0){xScoreCols[j]+=1;}
+                //Stoppa motståndare med 3 poäng
+                
                 //X Ensam på diagonal
                 int diT = isDiag(i, j);
                 if(diT!=2){
@@ -308,97 +322,110 @@ int gamma(int player, const GameState &pState){
     //Calculate nr rows&&cols&&diag with more than 2 - dead rows&&cols&&diag
     int good =0;
     int bad =0;
-    cerr<<"\nrx, cx, ry, cy \n";
+    //cerr<<"\nrx, cx, ry, cy \n";
     for(int k=0;k<4;++k){
         //Score +1 bara om ensam på rad
         int rx = xScoreRows[k];
         int cx = xScoreCols[k];              
         int ry = yScoreRows[k];
         int cy = yScoreCols[k];
+        int rxd = xStopRowDanger[k];
+        int cxd = xStopColDanger[k];
+        int ryd = yStopRowDanger[k];
+        int cyd = yStopColDanger[k];
         
-        cerr<<" "<<rx<<"  "<<cx<<"  "<<"  "<<ry<<"  "<<cy<<"\n";
-        if(player==0){
-            if(rx==1){good+=1;}
-            if(cx==1){good+=1;}
+        
+        //cerr<<" "<<rx<<"  "<<cx<<"  "<<"  "<<ry<<"  "<<cy<<"\n";
+        if(!player){
+            if(rx==1){good+=0;}
+            if(cx==1){good+=0;}
             if(rx==2){good+=5;}
             if(cx==2){good+=5;}
             if(rx==3){good+=10;}
             if(cx==3){good+=10;}
-            if(rx==4){good+=INT8_MAX;}
-            if(cx==4){good+=INT8_MAX;}
+            if(rx==4){good+=1000;}
+            if(cx==4){good+=1000;}
+            if(rxd==1){good+=100;}
+            if(cxd==1){good+=100;}
             
-//            if(ry==1){bad+=10;}
-//            if(cy==1){bad+=10;}
-//            if(ry==2){bad+=50;}
-//            if(cy==2){bad+=50;}
-//            if(ry>2){bad+=100;}
-//            if(cy>2){bad+=100;}
-//            if(ry>3){bad+=INT8_MAX;}
-//            if(cy>3){bad+=INT8_MAX;}
-        }else if(player==1){
-//            if(rx==1){bad+=10;}
-//            if(cx==1){bad+=10;}
-//            if(rx==2){bad+=50;}
-//            if(cx==2){bad+=50;}
-//            if(rx==3){bad+=100;}
-//            if(cx==3){bad+=100;}
-//            if(rx==4){bad+=INT8_MAX;}
-//            if(cx==4){bad+=INT8_MAX;}
+            if(ry==1){bad+=0;}
+            if(cy==1){bad+=0;}
+            if(ry==2){bad+=50;}
+            if(cy==2){bad+=50;}
+            if(ry==3){bad+=100;}
+            if(cy==3){bad+=100;}
+            if(ry==4){bad+=1000;}
+            if(cy==4){bad+=1000;}
+        }else if(player){
+            if(rx==1){bad+=0;}
+            if(cx==1){bad+=0;}
+            if(rx==2){bad+=50;}
+            if(cx==2){bad+=50;}
+            if(rx==3){bad+=100;}
+            if(cx==3){bad+=100;}
+            if(rx==4){bad+=1000;}
+            if(cx==4){bad+=1000;}
             
-            if(ry==1){good+=1;}
-            if(cy==1){good+=1;}
+            if(ry==1){good+=0;}
+            if(cy==1){good+=0;}
             if(ry==2){good+=5;}
             if(cy==2){good+=5;}
             if(ry==3){good+=10;}
             if(cy==3){good+=10;}
-            if(ry==4){good+=INT8_MAX;}
-            if(cy==4){good+=INT8_MAX;}
+            if(ry==4){good+=1000;}
+            if(cy==4){good+=1000;}
+            if(ryd==1){good+=100;}
+            if(cyd==1){good+=100;}
         }          
     }
     for(int l=0;l<2;++l){
-        int dx = xScoreDi[l];
-        
+        int dx = xScoreDi[l];       
         int dy = yScoreDi[l];
+        int dxd = xStopDiDanger[l];
+        int dyd = yStopDiDanger[l];
         //Player X
-        if(player==0){
+        if(!player){
             if(dx==1){good+=1;}
             if(dx==2){good+=5;}
             if(dx==3){good+=10;}
-            if(dx==4){good+=INT8_MAX;}
+            if(dx==4){good+=1000;}
+            if(dxd==1){good+=100;}
+            
 //            
-//            if(dy==1){bad+=1;}
-//            if(dy==2){bad+=5;}
-//            if(dy==3){bad+=10;}
-//            if(dy==4){bad+=INT8_MAX;}
+            if(dy==1){bad+=10;}
+            if(dy==2){bad+=50;}
+            if(dy==3){bad+=100;}
+            if(dy==4){bad+=1000;}
         //Player O
-        }else if(player==1){
-//            if(dx==1){bad+=1;}
-//            if(dx==2){bad+=5;}
-//            if(dx==3){bad+=10;}
-//            if(dx==4){bad+=INT8_MAX;}
+        }else if(player){
+            if(dx==1){bad+=10;}
+            if(dx==2){bad+=50;}
+            if(dx==3){bad+=100;}
+            if(dx==4){bad+=1000;}
 
             if(dy==1){good+=1;}
             if(dy==2){good+=5;}
             if(dy==3){good+=10;}
-            if(dy==4){good+=INT8_MAX;}
+            if(dy==4){good+=1000;}
+            if(dyd==1){good+=100;}
         }       
     }
     int chancesLeft=0;
-    if(player==0){
+    if(!player){
         chancesLeft = xRowChances+xColChances+xDiChances;
-    }else if(player==1){
+    }else if(player){
         chancesLeft = yRowChances+yColChances+yDiChances;
     }
 
-    cerr<<"\n good \n";
-    cerr<<good;
-    cerr<<"\n chances \n";
-    cerr<<chancesLeft<<"\n";
-    return good+(chancesLeft*100);
+    //cerr<<"\n good \n";
+    //cerr<<good;
+    //cerr<<"\n chances \n";
+    //cerr<<chancesLeft<<"\n";
+    return good-bad+(chancesLeft*50);
     }
 
 //Spelare A=1, Spelare B=0
-int minMax(const GameState &pState, int player, int depth, int alfa, int beta){
+int minMax(const GameState &pState, bool playerBool, int depth, int alfa, int beta){
     //cerr<<"\n------minMax----\n";
     
     
@@ -406,17 +433,17 @@ int minMax(const GameState &pState, int player, int depth, int alfa, int beta){
     double bestPossible;
     int v;
     if(depth==0||mu(pState).size()==0){
-        v= (gamma(player, pState));
+        v= (gamma(playerBool, pState));
         //cerr<<"gamma: "<<v<<" \n";
         //cerr<<"player: "<<player<<" \n";
-    }else if(player == 0){
+    }else if(playerBool){
         //X-player
         v = INT8_MIN;//Blev problems med -INFINITY
         vector<GameState> States1 = mu(pState);
         for(int i=0;i<States1.size();++i){
             GameState child = States1[i];
 
-            v = max(v,minMax(child, 1, depth-1, alfa, beta));
+            v = max(v,minMax(child, false, depth-1, alfa, beta));
             //cerr<<"max; "<<v<<" \n";
             alfa = max(alfa, v);
             if(beta<=alfa){
@@ -430,7 +457,7 @@ int minMax(const GameState &pState, int player, int depth, int alfa, int beta){
         vector<GameState> States2 = mu(pState);
         for(int j=0;j<States2.size();++j){
             GameState child = States2[j];
-            v = min(v, minMax(child, 0, depth-1, alfa, beta));
+            v = min(v, minMax(child, true, depth-1, alfa, beta));
             //cerr<<"min; "<<v<<" \n";
             beta = min(beta, v);
             if(beta<=alfa){
@@ -447,16 +474,20 @@ int minMax(const GameState &pState, int player, int depth, int alfa, int beta){
 GameState Player::play(const GameState &pState,const Deadline &pDue)
 {
     //std::cerr << "Processing " << pState.toMessage() << std::endl;
-    //cerr<<"\n------play----\n";
-    int player;
-    if(lastPlayer==1){
-        player=0;
-        lastPlayer =0;
-    }else{
-        player=1;
-        lastPlayer=1;
-    }
-    
+    //int player;
+//    cerr<<"\n------play----\n";
+//    cerr<<"\n player before: "<<player<<"\n";
+//    
+//    if(lastPlayer==1){
+//        player=0;
+//        lastPlayer =0;
+//        cerr<<"\n last player was 1 \n";
+//    }else if(lastPlayer==0){
+//        player=1;
+//        lastPlayer=1;
+//        cerr<<"\n last player was 0 \n";
+//    }
+//    cerr<<"\n player after: "<<player<<"\n";
     vector<GameState> lNextStates = mu(pState);
     if (lNextStates.size() == 0) return GameState(pState, Move());
 
@@ -467,8 +498,8 @@ GameState Player::play(const GameState &pState,const Deadline &pDue)
     //return lNextStates[rand() % lNextStates.size()];
     int max = 0;
     for(int i=0;i<lNextStates.size();++i){
-       // cerr<<"i: "<<i<<" ";
-        int newMax = minMax(lNextStates[i], player, INT8_MAX, INT8_MAX, INT8_MIN);
+        
+        int newMax = minMax(lNextStates[i], true, INT8_MAX, INT8_MAX, INT8_MIN);
         if(newMax>max){
             max = i;
         }
