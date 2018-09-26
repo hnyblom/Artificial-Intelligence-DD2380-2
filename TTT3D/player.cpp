0e525071c8a5ee6a0348eb1a9ff6a7210b2720c7
Nicole Jagelid <nicolejagelid@Nicoles-MacBook-Pro.local>
2018-09-26 18:10:48 +0200

    15p on kattis

--- TTT3D/player.cpp
+++ TTT3D/player.cpp
@@ -1,8 +1,72 @@
 #include "player.hpp"
 #include <cstdlib>
+#include <cmath>
+#include <tuple>
+#include <vector>
+#include <algorithm> 
+#include <limits>
+#include <climits>
+#include <cstdint>
 
-namespace TICTACTOE3D
-{
+/*
+By: Hanna & Nicole
+*/
+
+namespace TICTACTOE3D{
+using namespace std;
+
+vector<GameState> mu(const GameState &pState){
+    std::vector<GameState> lNextStates;
+    pState.findPossibleMoves(lNextStates);
+    return lNextStates;
+}
+
+int gamma(bool player, const GameState &pState){
+    if(pState.isXWin()){
+        return 2;
+    }
+    else if(pState.isOWin()){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+
+    
+}
+
+//Spelare A=1, Spelare B=0
+int minMax(const GameState &pState, bool playerBool, int depth, int alfa, int beta){
+    int v;
+    if(pState.isEOG() || depth == 0){
+        v=(gamma(playerBool, pState));
+    }else if(playerBool){
+        //X-player
+        v = INT_MIN;
+        vector<GameState> States1 = mu(pState);
+        for(int i=0;i<States1.size();++i){
+            GameState child = States1[i];
+            v = max(v,minMax(child, false, depth-1, alfa, beta));
+            alfa = max(alfa, v);
+            if(beta<=alfa){
+                break;
+            }
+        }
+    //O-player
+    }else{
+        v = INT_MAX;
+        vector<GameState> States2 = mu(pState);
+        for(int j=0;j<States2.size();++j){
+            GameState child = States2[j];
+            v = min(v, minMax(child, true, depth-1, alfa, beta));
+            beta = min(beta, v);
+            if(beta<=alfa){
+                break;
+            }
+        }
+    }
+    return v;
+}
 
 GameState Player::play(const GameState &pState,const Deadline &pDue)
 {
@@ -19,7 +83,16 @@ GameState Player::play(const GameState &pState,const Deadline &pDue)
      * next state. This skeleton returns a random move instead.
      */
     
-    return lNextStates[rand() % lNextStates.size()];
+    int max = 0;
+    int stateIndex=0;
+    for(int i=0;i<lNextStates.size();++i){
+        int newMax = minMax(lNextStates[i], false, 2, INT_MIN, INT_MAX);
+        if(newMax>max){
+            max=newMax;
+            stateIndex = i;
+        }
+    }
+    return lNextStates[stateIndex];
 }
 
 /*namespace TICTACTOE3D*/ }
